b139b931d2c306c590dfcd7b686b5f2a3bb4e36e
John <john@hushkeys.com>
2018-07-04 17:42:00 +0100

    Home page setup and styled

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import { Route } from 'react-router-dom';
 import Home from './components/pages/home'
 import Moon from './components/pages/moon';
 import View from './components/pages/view';
+import './App.css';
 
 const App = () => (
-      <div>
+      <div className="app">
         <Route path="/" exact component={Home} />
         <Route path="/Moon" exact component={Moon} />
         <Route path="/View" exact component={View} />
--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -4,8 +4,10 @@ import './home.css';
 
 const Home = () => (
   <div className="home">
-    <Link className="moonLink" to="/Moon">Copy</Link>
-    <Link className="viewLink" to="/View">Digital</Link>
+    <Link className="moonLink" to="/Moon"></Link>
+    <Link className="viewLink" to="/View"></Link>
+    <div className="moonText" />
+    <div className="viewText" />
   </div>
 );
 
