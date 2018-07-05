a12363cac9994ea5b2083ddfec20b78bea3d632e
John <john@hushkeys.com>
2018-07-05 18:07:53 +0100

    valitation and inline erros

--- src/components/pages/login.js
+++ src/components/pages/login.js
@@ -1,39 +1,65 @@
 import React from 'react';
 import { Form, Button } from 'semantic-ui-react';
 import Validator from 'validator';
+import InlineError from '../elements/messages/inlineError.js';
 
 class Login extends React.Component {
   state = {
-    data: {},
+    data: {
+      email: '',
+      password: ''
+    },
     loading: false,
-    error: {}
+    errors: {}
   };
+
+  onChange = e => this.setState ({ data: { ...this.state.data, [e.target.name]: e.target.value} });
+
+  onSubmit = () => {
+    const errors = this.validate(this.state.data);
+    this.setState({ errors });
+    if(Object.keys(errors).length === 0){
+      this.props.submit(this.state.data);
+    }
+  }
+
+  validate = (data) => {
+    const errors = {};
+    if (!Validator.isEmail(data.email)) errors.email = "Invalid email!";
+    if (!data.password) errors.password = "Password was incorrect!";
+    return errors;
+  }
+
   render() {
+    const { data, errors } = this.state;
     return (
       <div>
         <Form onSubmit={this.onSubmit}>
-          <Form.Field>
+          <Form.Field error={!!errors.email}>
             <label htmlFor="email">Email</label>
             <input
-              typeof="email"
+              type="email"
               id="email"
               name="email"
-              placeholder="Email"
+              placeholder="example@example.com"
               value={data.email}
               onChange={this.onChange}
             />
+            {errors.email && <InlineError text={ errors.email} />}
           </Form.Field>
-          <Form.Field>
+          <Form.Field error={!!errors.password}>
                 <label htmlFor="password">Password</label>
                 <input
-                  typeof="password"
+                  type="password"
                   id="password"
                   name="password"
                   placeholder="Password"
                   value={data.password}
                   onChange={this.onChange}
                 />
+                {errors.password && <InlineError text={ errors.password} />}
           </Form.Field>
+          <Button primary>Login</Button>
         </Form>
       </div>
     );
